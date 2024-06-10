439e2dc8780deca4dcaf0a517f6026f805d9fb06
CC <caledoniaconsult@gmail.com>
2024-06-10 18:36:47 -0400

    wip - css cleanup about

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -12,31 +12,30 @@ const props = defineProps(['width'])
 </script>
 
 <template>
-  <div class="main" v-if="props.width > 900">
-    <div>
-      <div class="hero-text-container">
-        <div class="hero-text-sentence" v-for="word in heroText">
-          <div class="hero-text-word">{{word}}</div>
+  <div class="container__desktop" v-if="props.width > 900">
+    <div class="container__left">
+      <div class="container__left--herotext">
+        <div class="container__left--herotext-sentence" v-for="word in heroText">
+          <div class="">{{word}}</div>
         </div>
       </div>
       <img
-          class="portrait"
+          class="container__left--portrait"
           src="https://images.squarespace-cdn.com/content/v1/5810dda3e3df28ce37b58357/1635801209439-FFUCZLQKLS0TSL2CJ33G/signal-2021-11-01-160737_001.jpg?format=1000w"
           width="100%"
           loading="lazy"
           height="100%"
           alt="Large image of Dean Pinkert"
       />
-      <span class="overlay"></span>
     </div>
-    <div class="text">
-      <p>An Innovator in international trade and human rights policy. Since departing from the
+    <div class="container__bio">
+      <p class="container__bio--content">An Innovator in international trade and human rights policy. Since departing from the
         International Trade Commission (“ITC”) in 2017 after ten years as a Commissioner (with two
         years as presidentially designated Vice Chairman), involvement in public debate has included
         testimony before the ITC and an interagency forced labor committee, televised interviews, and
         publications on a range of topics including human rights violations in U.S.-facing supply
         chains.</p>
-      <div class="cta-container">
+      <div class="container__cta--desktop">
         <audio controls>
           <source src="https://audio.jukehost.co.uk/fHzavDGfQfonj9VuDlpnaUizgjZt9fm2">
         </audio>
@@ -45,6 +44,8 @@ const props = defineProps(['width'])
       </div>
 
     </div>
+    <span class="overlay"></span>
+
   </div>
   <div class="main-mobile" v-else>
     <div class="hero-text-container-mobile">
@@ -80,9 +81,112 @@ const props = defineProps(['width'])
 </template>
 
 <style scoped>
-  .main {
-    height: 100%;
+
+  .container__desktop {
+    min-height: 100vh;
     width: 100%;
+
+    .container__left {
+
+      .container__left--herotext {
+        position: absolute;
+        bottom: 5px;
+        height: 90vh;
+        width: 55vw;
+        z-index: 1;
+        margin: 0 0 0 15px;
+        font-size: 85px;
+      }
+
+      .container__left--herotext-sentence {
+        font-size: 1em;
+      }
+
+      .container__left--herotext-sentence:nth-child(1) {
+        color: #6dcbdf;
+      }
+
+      .container__left--herotext-sentence:nth-child(2) {
+        color: #eaeadd;
+      }
+
+      .container__left--herotext-sentence:nth-child(3) {
+        color: #fec452;
+      }
+
+      .container__left--herotext-sentence:nth-child(4) {
+        color: #f65147;
+      }
+
+      .container__left--portrait {
+        width: 100%;
+        position: absolute;
+        height: 100%;
+        object-fit: cover;
+      }
+
+
+    }
+
+    .container__bio {
+      position: absolute;
+      margin: 35% 25px 0 0;
+      right: 0;
+      padding: 5px;
+      border-radius: 8px;
+      z-index: 2;
+      color: white;
+      font-weight: 100;
+      font-size: 16px;
+      width: 42vw;
+      line-height: 1.8em;
+    }
+
+    .container__bio--content {
+      background: black;
+      padding: 10px;
+    }
+
+    .container__cta--desktop {
+      display: flex;
+      flex-direction: row;
+
+      .linkedin {
+        margin-left:  15px;
+        display: flex;
+        align-self: center;
+
+        a {
+          color: #f65147;
+        }
+
+
+      }
+
+      .mailto {
+        margin-left:  15px;
+        display: flex;
+        align-self: center;
+
+        a {
+          color: #f65147;
+        }
+
+      }
+
+    }
+
+    .overlay {
+      transition: .6s ease;
+      opacity: 0.7;
+      box-shadow:  2px 1px 6px 0 #949494;
+      position: absolute;
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.86), rgba(0, 0, 0, 0.86));
+      height: 100%;
+      width: 100%;
+      text-align: center;
+    }
+
   }
 
   .main-mobile {
@@ -161,114 +265,46 @@ const props = defineProps(['width'])
         }
       }
     }
-  }
 
-  .text {
-    position: absolute;
-    margin: 35% 25px 0 0;
-    right: 0;
-    padding: 5px;
-    border-radius: 8px;
-    z-index: 2;
-    color: white;
-    font-weight: 100;
-    font-size: 16px;
-    width: 42vw;
-    line-height: 1.8em;
-  }
-
-  #about {
-    display: flex;
-    flex-direction: row;
-
-    .portrait {
-      width: 100%;
+    .overlay {
+      transition: .6s ease;
+      opacity: 0.7;
+      box-shadow:  2px 1px 6px 0 #949494;
       position: absolute;
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.86), rgba(0, 0, 0, 0.86));
       height: 100%;
-      object-fit: cover;
-    }
-  }
-
-  .overlay {
-    transition: .6s ease;
-    opacity: 0.7;
-    box-shadow:  2px 1px 6px 0 #949494;
-    position: absolute;
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.86), rgba(0, 0, 0, 0.86));
-    height: 100%;
-    width: 100%;
-    text-align: center;
-
-    .title {
-      font-weight: 600;
+      width: 100%;
+      text-align: center;
     }
 
-    .linkout {
-      font-weight: 400;
-      text-decoration: underline;
-    }
-  }
-
-  .hero-text-container {
-    position: absolute;
-    height: 90vh;
-    width: 55vw;
-    z-index: 1;
-    margin: 0 0 0 15px;
-    bottom: 5px;
-    font-size: 85px;
-  }
-
-  .hero-text-sentence {
-    color: white;
-  }
-
-  .hero-text-word {
-    font-size: 1em;
-  }
-
-  .hero-text-sentence:nth-child(1) {
-    color: #6dcbdf;
-  }
-
-  .hero-text-sentence:nth-child(2) {
-    color: #eaeadd;
-  }
-
-  .hero-text-sentence:nth-child(3) {
-    color: #fec452;
-  }
-
-  .hero-text-sentence:nth-child(4) {
-    color: #f65147;
-  }
-
-  .cta-container {
-    display: flex;
-    flex-direction: row;
-  }
-
-  .linkedin {
-    margin-left:  15px;
-    display: flex;
-    align-self: center;
-
-    a {
-      color: #f65147;
+    .cta-container {
+      display: flex;
+      flex-direction: row;
     }
 
 
-  }
+    .linkedin {
+      margin-left:  15px;
+      display: flex;
+      align-self: center;
 
-  .mailto {
-    margin-left:  15px;
-    display: flex;
-    align-self: center;
+      a {
+        color: #f65147;
+      }
 
-    a {
-      color: #f65147;
+
     }
 
+    .mailto {
+      margin-left:  15px;
+      display: flex;
+      align-self: center;
+
+      a {
+        color: #f65147;
+      }
+
+    }
   }
 
 </style>
